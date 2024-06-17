d316f1dc71093051d6c64d684829e60b0eaedd62
Marcos Vinícius Jacometti <marcosjacometti10@gmail.com>
2024-06-17 10:20:57 -0300

    bets page

--- frontend/src/pages/bets/index.js
+++ frontend/src/pages/bets/index.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { BetsContainer } from "./styles";
+import { BetsContainer, Container, Games, GamesContent } from "./styles";
 import Header from "../../components/common/header";
+import { IoExtensionPuzzleOutline } from "react-icons/io5";
+import Game from "../../components/specific/games";
 
 export default function Bets(){
     return(
         <BetsContainer>
             <Header />
+            <GamesContent>
+                <Container>
+                    <div className="title">
+                        <IoExtensionPuzzleOutline />
+                        <h2>Sinais Automáticos</h2>
+                    </div>
+                    <Games>
+                        <Game />
+                    </Games>
+                </Container>
+            </GamesContent>
         </BetsContainer>
     );
 }
