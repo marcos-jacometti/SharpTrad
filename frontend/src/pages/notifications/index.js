1ff33de9a61eb22d9dfe3b7db075b346f544f5dd
Marcos Vinícius Jacometti <marcosjacometti10@gmail.com>
2024-06-19 11:30:44 -0300

    notifications page

--- frontend/src/pages/notifications/index.js
+++ frontend/src/pages/notifications/index.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Notify, NotifyContainer } from "./styles";
 import Header from "../../components/common/header";
+import { MdOutlineNotificationsActive } from "react-icons/md";
 
 export default function Notifications(){
     return(
         <Container>
             <Header />
+            <NotifyContainer>
+                <Notify>
+                    <div className="title">
+                        <MdOutlineNotificationsActive />
+                        <h2>Notificações</h2>
+                    </div>
+                </Notify>
+            </NotifyContainer>
         </Container>
     );
 }
--- frontend/src/pages/notifications/styles.js
+++ frontend/src/pages/notifications/styles.js
@@ -3,3 +3,36 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
 `;
+
+export const NotifyContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 82vw;
+    height: 100vh;
+`;
+
+export const Notify = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 70vw;
+    background-color: #fff;
+    border-radius: 1vh;
+    height: 90vh;
+
+    .title {
+        display: flex;
+        align-items: center;
+        padding: 1rem;
+        gap: 1rem;
+        display: flex;
+        width: 68vw;
+        height: 10vh;
+        border-bottom: 0.1vh solid #e7e8eb;
+
+        svg {
+            font-size: 1.5vw;
+        }
+    }
+`;
