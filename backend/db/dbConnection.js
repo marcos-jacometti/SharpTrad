91df2b04f38f754d378a7a415daa6491a0505cf9
Marcos Vinícius Jacometti <marcosjacometti10@gmail.com>
2024-07-10 16:58:53 -0300

    dbname

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
+console.log('DB_NAME:', process.env.DB_NAME);
 const connection = mysql.createConnection(urlDB);
 
 const dbConnection = mysql.createConnection(urlDB);
